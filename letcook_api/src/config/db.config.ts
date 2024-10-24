9c4d61e0f2bc2436e494548e477c80d0a1cca63b
Rich-100trieu <thuyntt191102@gmail.com>
2024-10-24 20:48:34 +0700

    share and follow v2

--- letcook_api/src/config/db.config.ts
+++ letcook_api/src/config/db.config.ts
@@ -31,7 +31,7 @@ import { Favorite } from "@/entity/favourite.entity";
 // import env from '@/util/validateEnv';
 const MongoDataSource = new DataSource({
   type: "mongodb",
-  // url: `mongodb+srv://${env.MONGO_USER}:${env.MONGO_PASS}@${env.MONGO_HOST}/${env.MONGO_DB}?retryWrites=true&w=majority`,
+  // url: `mongodb://localhost:27017/api`,
   url: `${env.LOCAL_MONGO}`,
 
   // host: env.MONGO_HOST,
