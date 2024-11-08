af6975893e3271d7e980718aafe318f04f1290b7
Pickky03 <vhai02092003@gmail.com>
2024-11-08 20:24:10 +0700

    update video

--- letcook_api/src/config/db.config.ts
+++ letcook_api/src/config/db.config.ts
@@ -67,7 +67,7 @@ const MongoDataSource = new DataSource({
 const PostgresDataSource = new DataSource({
   type: "postgres",
   // url: env.POSTGRES_EXTERNAL_URL,
-  url: "postgresql://postgres:1121212@localhost:5432/letcook",
+  url: "postgresql://postgres:123@localhost:5432/letcook",
   // ssl: {
   //   rejectUnauthorized: false, // This is important if you don't have a custom CA
   // },
